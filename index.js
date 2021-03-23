3296075f04603aef9a79b2e5a3dc53e1bd9a2a68
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-23 19:30:46 +0600

    dynamic api, api parameter, access params, access query

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express');
 
 const app = express();
-/*
-// normal function
-function rootCall(req, res) {
-    res.send('Thank You....')
-}
-// arrow function
-const rootCall = (req, res) => res.send('Thank You....');
-app.get('/', rootCall)
-*/
+
+const users = ['samiul', 'Hasan', 'Nayeem', 'Forhad', 'Susmita', 'Mafruha'];
 
 app.get('/', (req, res) => {
     const fruit = {
@@ -21,6 +14,13 @@ app.get('/', (req, res) => {
 
 app.get('/fruits/orange', (req, res) => {
     res.send({ fruit: 'orange', quantity: 1000, price: 10000 });
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.query.sort);
+    const name = users[id];
+    res.send({ id, name });
 })
 
 app.listen(3000, () => console.log('Listening to port 3000'));
