ebda7dfad677a461cc3ceff0732dcca5907de4b2
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-24 15:20:59 +0600

    create POST api, install postman, read post data, use body parser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 const users = ['samiul', 'Hasan', 'Nayeem', 'Forhad', 'Susmita', 'Mafruha'];
 
@@ -18,11 +20,17 @@ app.get('/fruits/orange', (req, res) => {
     res.send({ fruit: 'orange', quantity: 1000, price: 10000 });
 })
 
+// get method
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
     console.log(req.query.sort);
     const name = users[id];
     res.send({ id, name });
+})
+
+// post method
+app.post('/addUser', (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(3000, () => console.log('Listening to port 3000'));
